70910f2bf4915c883aa420964cd09d0002998a18
Tudzy <narongsak5az@gmail.com>
2024-01-22 09:36:02 +0700

    .

--- lib/playlist_page.dart
+++ lib/playlist_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:qr_challenge/playlist_data.dart';
 
-class PlaylistPage extends StatelessWidget {
-  const PlaylistPage({super.key});
+class PlaylistPage extends StatefulWidget {
+  const PlaylistPage({Key? key}) : super(key: key);
 
   @override
+  State<PlaylistPage> createState() => _PlaylistPageState();
+}
+
+class _PlaylistPageState extends State<PlaylistPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PlayList'),
+      ),
+      body: ListView.builder(
+        itemCount: playlist.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: const Icon(Icons.video_file),
+            title: Text(playlist[index]['title']!),
+            subtitle: Text(playlist[index]['url']!),
+            trailing: ElevatedButton(
+              child: const Icon(Icons.qr_code),
+              onPressed: () {
+                Get.toNamed("/qr", arguments: playlist[index]);
+              },
+            ),
+            onTap: () async {
+              await _launchUrl(playlist[index]['url']!);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  // Assuming you have the _launchUrl method defined somewhere
+  Future<void> _launchUrl(String url) async {
+    // Implementation of launching the URL
   }
 }
--- lib/qr_page.dart
+++ lib/qr_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_challenge/playlist_data.dart';
 
 class QrPage extends StatelessWidget {
-  const QrPage({super.key});
+  final Map<String, String> playlistItem;
+
+  const QrPage({Key? key, required this.playlistItem}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var index = Get.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            index['title'] ?? 'Title'), // Use the title from the playlistItem
+      ),
+      body: Center(
+        child: QrImageView(
+          data: index['url'],
+          version: QrVersions.auto,
+          size: 200.0,
+        ),
+      ),
+    );
   }
 }
